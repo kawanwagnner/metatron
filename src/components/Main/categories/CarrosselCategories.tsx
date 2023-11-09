a51419ac3d9b18cbf76091d050ab503d92a404f5
unknown <kawanwagnner7@gmail.com>
2023-11-09 15:31:12 -0300

    feat: remove corroussel motion

--- src/components/Main/categories/CarrosselCategories.tsx
+++ src/components/Main/categories/CarrosselCategories.tsx
@@ -1,34 +1,9 @@
-import React, { useState } from "react";
 import { motion } from "framer-motion";
 import CategoriesItems from "./CategoriesItems";
 
 import "./css/Categories.css";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 const Categories: React.FC = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const slides = [
-    <div className="category" />,
-    <div className="category" />,
-    <div className="category" />,
-  ];
-
-  const slideWidth = 260; // Defina a largura do slide aqui
-
-  const trackStyle = {
-    width: `${slides.length * slideWidth}px`,
-    transform: `translate3d(-${currentSlide * slideWidth}px, 0, 0)`,
-    transition: "transform 0.5s ease",
-  };
-
-  const nextSlide = () => {
-    setCurrentSlide((currentSlide + 1) % slides.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentSlide((currentSlide - 1 + slides.length) % slides.length);
-  };
 
   return (
     <div id="container" className="categories">
@@ -43,38 +18,11 @@ const Categories: React.FC = () => {
           Nossas categorias:
         </motion.h3>
 
-        <motion.div className="carousel-container">
-          <div className="carousel-track" style={trackStyle}>
-            {slides.map((_, index) => (
-              <motion.div
-                key={index}
-                className="carousel-slide"
-                initial={{ opacity: 0, x: 100 }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: -100 }}
-                transition={{ duration: 0.5 }}
-              >
-                <CategoriesItems />
-              </motion.div>
-            ))}
+        <div className="carousel-container">
+          <div className="carousel">
+            <CategoriesItems />
           </div>
-          <button
-            className="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-fullWidth MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-fullWidth css-ukcdk3 button-prev"
-            onClick={prevSlide}
-          >
-            <MdKeyboardArrowLeft size={30} />
-          </button>
-          <button
-            className="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-fullWidth MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-fullWidth css-ukcdk3 button-next"
-            onClick={nextSlide}
-          >
-            <MdKeyboardArrowRight size={30} />
-          </button>
-        </motion.div>
-
-        {/* <button className="btn-show-all" type="button">
-          Mostrar mais categorias
-        </button> */}
+        </div>
       </div>
     </div>
   );
